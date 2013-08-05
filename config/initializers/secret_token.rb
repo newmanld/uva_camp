b8ebb361ffc1fe73bc088118d1aa5f712faea8bf
Linda Newman <newmanld@ucmail.uc.edu>
2013-08-05 14:57:56 -0400

    uva_camp created at hydra camp

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+UvaCamp::Application.config.secret_key_base = '8a347012db81cbc0245a351a9a07b0b8ce78ae226d2ab8b94d716e6187e348d3ece73cfe5c0d6603bbaefe30cc9c92f521bd7a2473008144dc3094ecddf6bee1'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+UvaCamp::Application.config.session_store :cookie_store, key: '_uva_camp_session'
