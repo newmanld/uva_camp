fbb58886f349666c3e269ba7200261c8ea60122e
Thomas Scherz <thomas.scherz@uc.edu>
2013-08-08 12:17:21 -0400

    Fixed Collection Facet Display

--- app/controllers/catalog_controller.rb
+++ app/controllers/catalog_controller.rb
@@ -99,7 +99,7 @@ class CatalogController < ApplicationController
     config.add_facet_field solr_name("desc_metadata__based_near", :facetable), :label => "Location", :limit => 5
     config.add_facet_field solr_name("desc_metadata__publisher", :facetable), :label => "Publisher", :limit => 5
     config.add_facet_field solr_name("file_format", :facetable), :label => "File Format", :limit => 5
-    config.add_facet_field Solrizer.solr_name("collection", :facetable), :label => "Collection",  :helper_method => :collection_title_from_pid,  :limit => 5
+    config.add_facet_field solr_name("collection", :facetable), :label => "Collection",  :helper_method => :collection_title_from_pid,  :limit => 5
 
     # Have BL send all facet field names to Solr, which has been the default
     # previously. Simply remove these lines if you'd rather use Solr request
@@ -368,7 +368,7 @@ class CatalogController < ApplicationController
   # @param user_parameters the current user-subitted parameters
   def exclude_unwanted_models(solr_parameters, user_parameters)
     solr_parameters[:fq] ||= []
-    solr_parameters[:fq] << "#{Solrizer.solr_name("has_model", :symbol)}:\"info:fedora/afmodel:GenericFile\""
+    solr_parameters[:fq] << "#{Solrizer.solr_name("has_model", :symbol)}:\"info:fedora/afmodel:GenericFile\" OR #{Solrizer.solr_name("has_model", :symbol)}:\"info:fedora/afmodel:Collection\""
   end
 
   def depositor 
